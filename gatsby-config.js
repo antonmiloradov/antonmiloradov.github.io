bfd78b0d8a127a4820ac235e9403e4d5bdeab39c
Artem Tyurin <artem.tyurin@uber.com>
2017-12-03 23:17:27 +0100

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-110555375-1`,
       },
     },
     `gatsby-plugin-feed`,
